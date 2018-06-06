d6589c04f2a21706a59cb6a291904823075262a7
Joe Maller <joe@joemaller.com>
2018-06-06 17:13:31 -0400

    add ShowIncludes to library

---
+++ Debug/ShowIncludes.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ideasonpurpose\Debug;
+
+class ShowIncludes
+{
+    public function __construct()
+    {
+        if (defined('WP_DEBUG') && WP_DEBUG) {
+            add_action('wp_footer', [$this, 'show']);
+        }
+    }
+
+    public function getFiles()
+    {
+        $includes = get_included_files();
+        $templates = array_filter($includes, function ($t) {
+            global $template;
+            // return (strpos($t, get_template_directory()) !== false) && ($t != $template);
+            return (strpos($t, get_template_directory()) !== false);
+        });
+        $this->templates = array_map(function ($t) {
+            global $template;
+            $t = ($t == $template) ? "<span class='template' style='font-weight: bold; color: #000;'>$t</span>" : $t;
+            return '<li style="list-style-type: none; color: #888; margin: 0;">' . str_replace(get_template_directory(), '', $t) . '</li>';
+        }, $templates);
+
+        global $template;
+        $this->templateName = str_replace(get_template_directory(), '', $template);
+    }
+
+    public function show()
+    {
+        $this->getFiles();
+        ?>
+        <div class="container" id="iop-wp-debug-template" style="font-size: 14px; color: #400; overflow: auto; margin: 3em auto; padding: 15px; border: 1px solid #ccc; border-radius: 4px; background: #eee; max-width: 90%;">
+          <strong>Template: <span style="font-family: monospace; font-size: 1.1em;"><?= $this->templateName ?></span></strong><br>
+          Included Files:
+          <ul style="padding-left: .5em; font-family: monospace; font-size: 1.1em; display: block; margin: 0.5em 0; white-space: nowrap;">
+            <?= implode("\n", $this->templates); ?>
+          </ul>
+        </div>
+        <?php
+    }
+}

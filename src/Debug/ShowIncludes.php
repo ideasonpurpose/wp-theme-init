0c0b90ac862d87146b203ec93f1a839f969b6969
Joe Maller <joe@joemaller.com>
2018-08-17 19:31:56 -0700

    Change ShowIncludes to use console.log instead of printing to the page

--- src/Debug/ShowIncludes.php
+++ src/Debug/ShowIncludes.php
@@ -10,45 +10,48 @@ class ShowIncludes
         }
     }
 
-    public function getFiles()
-    {
-        $includes = get_included_files();
-        $templates = array_filter($includes, function ($t) {
-            global $template;
-            // return (strpos($t, get_template_directory()) !== false) && ($t != $template);
-            return (strpos($t, get_template_directory()) !== false);
-        });
-        $this->templates = array_map(function ($t) {
-            global $template;
-            $t = ($t == $template)
-                ? "<span class='template' style='font-weight: bold; color: #000;'>$t</span>"
-                : $t;
-            return (
-                '<li style="list-style-type: none; color: #888; margin: 0;">' .
-                str_replace(get_template_directory(), '', $t) .
-                '</li>'
-            );
-        }, $templates);
+    // public function getFiles()
+    // {
+    //     $includes = get_included_files();
+    //     $templates = array_filter($includes, function ($t) {
+    //         global $template;
+    //         // return (strpos($t, get_template_directory()) !== false) && ($t != $template);
+    //         return (strpos($t, get_template_directory()) !== false);
+    //     });
+    //     $this->templates = array_map(function ($t) {
+    //         global $template;
+    //         $t = ($t == $template)
+    //             ? "<span class='template' style='font-weight: bold; color: #000;'>$t</span>"
+    //             : $t;
+    //         return (
+    //             // '<li style="list-style-type: none; color: #888; margin: 0;">' .
+    //             str_replace(get_template_directory(), '', $t)
+    //             // '</li>'
+    //         );
+    //     }, $templates);
 
-        global $template;
-        $this->templateName = str_replace(
-            get_template_directory(),
-            '',
-            $template
-        );
-    }
+    //     global $template;
+    //     $this->templateName = str_replace(
+    //         get_template_directory(),
+    //         '',
+    //         $template
+    //     );
+    // }
 
     public function show()
     {
-        $this->getFiles();
-        ?>
-        <div class="container" id="iop-wp-debug-template" style="font-size: 14px; color: #400; overflow: auto; margin: 3em auto; padding: 15px; border: 1px solid #ccc; border-radius: 4px; background: #eee; max-width: 90%;">
-          <strong>Template: <span style="font-family: monospace; font-size: 1.1em;"><?php echo $this->templateName; ?></span></strong><br>
-          Included Files:
-          <ul style="padding-left: .5em; font-family: monospace; font-size: 1.1em; display: block; margin: 0.5em 0; white-space: nowrap;">
-            <?php echo implode("\n", $this->templates); ?>
-          </ul>
-        </div>
-        <?php
+        global $template;
+        $includes = array_filter(get_included_files(), function ($t) {
+            return (strpos($t, get_template_directory()) !== false);
+        });
+
+        $files = [
+            'template' => str_replace(get_template_directory(), '', $template),
+            'included_files' => array_map(function ($t) {
+                return (str_replace(get_template_directory(), '', $t));
+            }, array_values($includes))
+        ];
+
+        printf('<script>console.log(%s);</script>', json_encode($files));
     }
 }

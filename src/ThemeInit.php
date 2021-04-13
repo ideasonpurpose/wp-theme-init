a05dd018059b61d7277c4eb930a20a3ada196e63
Joe Maller <joe@joemaller.com>
2021-04-13 14:35:15 -0400

    re-enable oEmbed stuff so embed block works

--- src/ThemeInit.php
+++ src/ThemeInit.php
@@ -89,13 +89,13 @@ class ThemeInit
         remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
         remove_action('wp_head', 'feed_links_extra', 3);
         remove_action('wp_head', 'print_emoji_detection_script', 7);
-        remove_action('wp_head', 'rest_output_link_wp_head', 10);
+        // remove_action('wp_head', 'rest_output_link_wp_head', 10);
         remove_action('wp_head', 'rsd_link');
         remove_action('wp_head', 'wlwmanifest_link');
         remove_action('wp_head', 'wp_generator');
-        remove_action('wp_head', 'wp_oembed_add_discovery_links');
-        remove_action('wp_head', 'wp_oembed_add_host_js');
-        remove_action('wp_head', 'wp_shortlink_wp_head', 10);
+        // remove_action('wp_head', 'wp_oembed_add_discovery_links');
+        // remove_action('wp_head', 'wp_oembed_add_host_js');
+        // remove_action('wp_head', 'wp_shortlink_wp_head', 10);
         remove_action('wp_print_styles', 'print_emoji_styles');
         remove_filter('comment_text_rss', 'wp_staticize_emoji');
         remove_filter('the_content_feed', 'wp_staticize_emoji');

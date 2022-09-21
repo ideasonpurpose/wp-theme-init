e84c55355e993049adfb13bab1901c2eeb5e60a5
Joe Maller <joe@ideasonpurpose.com>
2022-09-21 19:08:14 -0400

    breaking ListTable out to its own file

--- src/ThemeInit/Admin/TemplateAudit.php
+++ src/ThemeInit/Admin/TemplateAudit.php
@@ -1,10 +1,12 @@
 <?php
 namespace IdeasOnPurpose\ThemeInit\Admin;
 
+use IdeasOnPurpose\ThemeInit\Admin\TemplateAudit\ListTable;
+
 // @codeCoverageIgnoreStart
-if (!class_exists('WP_List_Table')) {
-    require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
-}
+// if (!class_exists('WP_List_Table')) {
+//     require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+// }
 // @codeCoverageIgnoreEnd
 
 // use \WP_List_Table;
@@ -144,131 +146,133 @@ class TemplateAudit
     }
 }
 
-/**
- * Customized subClass of WP_List_Table
- */
-class ListTable extends \WP_List_Table
-{
-    public function prepare_items()
-    {
-        $columns = $this->get_columns();
-        $hidden = $this->get_hidden_columns();
-        $sortable = $this->get_sortable_columns();
 
-        $user_id = get_current_user_id();
-        $screen = get_current_screen();
-        $screen_option = $screen->get_option('per_page', 'option');
 
-        $per_page = get_user_meta($user_id, $screen_option, true);
+// /**
+//  * Customized subClass of WP_List_Table
+//  */
+// class ListTable extends \WP_List_Table
+// {
+//     public function prepare_items()
+//     {
+//         $columns = $this->get_columns();
+//         $hidden = $this->get_hidden_columns();
+//         $sortable = $this->get_sortable_columns();
 
-        if (empty($per_page) || $per_page < 1) {
-            $per_page = $screen->get_option('per_page', 'default');
-        }
+//         $user_id = get_current_user_id();
+//         $screen = get_current_screen();
+//         $screen_option = $screen->get_option('per_page', 'option');
 
-        $data = $this->data();
-        usort($data, [$this, 'sortData']);
+//         $per_page = get_user_meta($user_id, $screen_option, true);
 
-        $currentPage = $this->get_pagenum();
-        $totalItems = count($data);
-        $this->set_pagination_args([
-            'total_items' => $totalItems,
-            'per_page' => $per_page,
-        ]);
+//         if (empty($per_page) || $per_page < 1) {
+//             $per_page = $screen->get_option('per_page', 'default');
+//         }
 
-        $data = array_slice($data, ($currentPage - 1) * $per_page, $per_page);
+//         $data = $this->data();
+//         usort($data, [$this, 'sortData']);
 
-        $this->_column_headers = [$columns, $hidden, $sortable];
-        $this->items = $data;
-    }
+//         $currentPage = $this->get_pagenum();
+//         $totalItems = count($data);
+//         $this->set_pagination_args([
+//             'total_items' => $totalItems,
+//             'per_page' => $per_page,
+//         ]);
 
-    public function get_columns()
-    {
-        $columns = [
-            'id' => 'ID',
-            'name' => 'Name',
-            'file' => 'File',
-            'count' => 'In Use',
-        ];
+//         $data = array_slice($data, ($currentPage - 1) * $per_page, $per_page);
 
-        return $columns;
-    }
+//         $this->_column_headers = [$columns, $hidden, $sortable];
+//         $this->items = $data;
+//     }
 
-    public function get_hidden_columns()
-    {
-        return ['id'];
-    }
+//     public function get_columns()
+//     {
+//         $columns = [
+//             'id' => 'ID',
+//             'name' => 'Name',
+//             'file' => 'File',
+//             'count' => 'In Use',
+//         ];
 
-    public function get_sortable_columns()
-    {
-        return ['name' => ['name', true], 'count' => ['count', false]];
-    }
+//         return $columns;
+//     }
 
-    public function data()
-    {
-        $theme = wp_get_theme();
-        $named_templates = $theme->get_page_templates();
+//     public function get_hidden_columns()
+//     {
+//         return ['id'];
+//     }
 
-        /**
-         * get all pages where the meta_key is set to _wp_page_template
-         */
-        $all_pages = get_pages(['meta_key' => '_wp_page_template']);
-        $templateCounts = [];
-        foreach ($all_pages as $page) {
-            $template = $page->meta_value;
-            if (!array_key_exists($template, $templateCounts)) {
-                $templateCounts[$template] = 0;
-            }
-            $templateCounts[$template]++;
-        }
+//     public function get_sortable_columns()
+//     {
+//         return ['name' => ['name', true], 'count' => ['count', false]];
+//     }
 
-        $i = 0;
-        $data = [];
-        foreach ($named_templates as $file => $name) {
-            $count = $templateCounts[$file] ?? 0;
-            $data[] = ['id' => $i++, 'name' => $name, 'file' => $file, 'count' => $count];
-        }
+//     public function data()
+//     {
+//         $theme = wp_get_theme();
+//         $named_templates = $theme->get_page_templates();
 
-        return $data;
-    }
+//         /**
+//          * get all pages where the meta_key is set to _wp_page_template
+//          */
+//         $all_pages = get_pages(['meta_key' => '_wp_page_template']);
+//         $templateCounts = [];
+//         foreach ($all_pages as $page) {
+//             $template = $page->meta_value;
+//             if (!array_key_exists($template, $templateCounts)) {
+//                 $templateCounts[$template] = 0;
+//             }
+//             $templateCounts[$template]++;
+//         }
 
-    /**
-     * Handle sorting of rows on either name or count
-     * @return int
-     */
-    public function sortData($a, $b)
-    {
-        $orderby = $_GET['orderby'] ?? 'name';
-        $order = $_GET['order'] ?? 'asc';
-        $result = 0;
+//         $i = 0;
+//         $data = [];
+//         foreach ($named_templates as $file => $name) {
+//             $count = $templateCounts[$file] ?? 0;
+//             $data[] = ['id' => $i++, 'name' => $name, 'file' => $file, 'count' => $count];
+//         }
 
-        switch ($orderby) {
-            case 'name':
-                $result = strcmp($a['name'], $b['name']);
-                break;
-            case 'count':
-                $result = $a['count'] <=> $b['count'];
-                break;
-            default:
-                break;
-        }
+//         return $data;
+//     }
 
-        return $order === 'asc' ? $result : -$result;
-    }
+//     /**
+//      * Handle sorting of rows on either name or count
+//      * @return int
+//      */
+//     public function sortData($a, $b)
+//     {
+//         $orderby = $_GET['orderby'] ?? 'name';
+//         $order = $_GET['order'] ?? 'asc';
+//         $result = 0;
 
-    public function column_default($item, $column_name)
-    {
-        switch ($column_name) {
-            case 'name':
-                return sprintf('<strong>%s</strong>', $item[$column_name]);
-                break;
-            case 'count':
-                return $item[$column_name] != 0 ? $item[$column_name] : '--';
-                break;
-            case 'id':
-            case 'file':
-            default:
-                return $item[$column_name];
-                break;
-        }
-    }
-}
+//         switch ($orderby) {
+//             case 'name':
+//                 $result = strcmp($a['name'], $b['name']);
+//                 break;
+//             case 'count':
+//                 $result = $a['count'] <=> $b['count'];
+//                 break;
+//             default:
+//                 break;
+//         }
+
+//         return $order === 'asc' ? $result : -$result;
+//     }
+
+//     public function column_default($item, $column_name)
+//     {
+//         switch ($column_name) {
+//             case 'name':
+//                 return sprintf('<strong>%s</strong>', $item[$column_name]);
+//                 break;
+//             case 'count':
+//                 return $item[$column_name] != 0 ? $item[$column_name] : '--';
+//                 break;
+//             case 'id':
+//             case 'file':
+//             default:
+//                 return $item[$column_name];
+//                 break;
+//         }
+//     }
+// }
---
+++ src/ThemeInit/Admin/TemplateAudit/ListTable.php
@@ -0,0 +1,137 @@
+<?php
+namespace IdeasOnPurpose\ThemeInit\Admin;
+
+// @codeCoverageIgnoreStart
+if (!class_exists('WP_List_Table')) {
+    require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+}
+// @codeCoverageIgnoreEnd
+
+/**
+ * Customized subClass of WP_List_Table
+ */
+class ListTable extends \WP_List_Table
+{
+    public function prepare_items()
+    {
+        $columns = $this->get_columns();
+        $hidden = $this->get_hidden_columns();
+        $sortable = $this->get_sortable_columns();
+
+        $user_id = get_current_user_id();
+        $screen = get_current_screen();
+        $screen_option = $screen->get_option('per_page', 'option');
+
+        $per_page = get_user_meta($user_id, $screen_option, true);
+
+        if (empty($per_page) || $per_page < 1) {
+            $per_page = $screen->get_option('per_page', 'default');
+        }
+
+        $data = $this->data();
+        usort($data, [$this, 'sortData']);
+
+        $currentPage = $this->get_pagenum();
+        $totalItems = count($data);
+        $this->set_pagination_args([
+            'total_items' => $totalItems,
+            'per_page' => $per_page,
+        ]);
+
+        $data = array_slice($data, ($currentPage - 1) * $per_page, $per_page);
+
+        $this->_column_headers = [$columns, $hidden, $sortable];
+        $this->items = $data;
+    }
+
+    public function get_columns()
+    {
+        $columns = [
+            'id' => 'ID',
+            'name' => 'Name',
+            'file' => 'File',
+            'count' => 'In Use',
+        ];
+
+        return $columns;
+    }
+
+    public function get_hidden_columns()
+    {
+        return ['id'];
+    }
+
+    public function get_sortable_columns()
+    {
+        return ['name' => ['name', true], 'count' => ['count', false]];
+    }
+
+    public function data()
+    {
+        $theme = wp_get_theme();
+        $named_templates = $theme->get_page_templates();
+
+        /**
+         * get all pages where the meta_key is set to _wp_page_template
+         */
+        $all_pages = get_pages(['meta_key' => '_wp_page_template']);
+        $templateCounts = [];
+        foreach ($all_pages as $page) {
+            $template = $page->meta_value;
+            if (!array_key_exists($template, $templateCounts)) {
+                $templateCounts[$template] = 0;
+            }
+            $templateCounts[$template]++;
+        }
+
+        $i = 0;
+        $data = [];
+        foreach ($named_templates as $file => $name) {
+            $count = $templateCounts[$file] ?? 0;
+            $data[] = ['id' => $i++, 'name' => $name, 'file' => $file, 'count' => $count];
+        }
+
+        return $data;
+    }
+
+    /**
+     * Handle sorting of rows on either name or count
+     * @return int
+     */
+    public function sortData($a, $b)
+    {
+        $orderby = $_GET['orderby'] ?? 'name';
+        $order = $_GET['order'] ?? 'asc';
+        $result = 0;
+
+        switch ($orderby) {
+            case 'name':
+                $result = strcmp($a['name'], $b['name']);
+                break;
+            case 'count':
+                $result = $a['count'] <=> $b['count'];
+                break;
+            default:
+                break;
+        }
+
+        return $order === 'asc' ? $result : -$result;
+    }
+
+    public function column_default($item, $column_name)
+    {
+        switch ($column_name) {
+            case 'name':
+                return sprintf('<strong>%s</strong>', $item[$column_name]);
+                break;
+            case 'count':
+                return $item[$column_name] != 0 ? $item[$column_name] : '--';
+                break;
+            case 'id':
+            case 'file':
+            default:
+                return $item[$column_name];
+                break;
+        }
+    }
+}

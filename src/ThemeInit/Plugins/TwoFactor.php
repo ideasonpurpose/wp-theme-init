74c279a9fb41a88605c4e8ccd89d15d6c687df20
Joe Maller <joe@joemaller.com>
2026-06-01 23:42:11 -0400

    disable Two Factor for development environments

--- src/ThemeInit/Plugins/TwoFactor.php
+++ src/ThemeInit/Plugins/TwoFactor.php
@@ -7,6 +7,7 @@ class TwoFactor
     {
         // Set the emailed codes to industry standard 6 characters.
         add_filter('two_factor_email_token_length', fn(): int => 6);
+        add_filter('two_factor_enabled_providers_for_user', [$this, 'disableForDev'], 999);
 
         if ($enforce_mfa) {
             add_filter('two_factor_enabled_providers_for_user', [$this, 'enforceEmail']);
@@ -23,5 +24,17 @@ class TwoFactor
     public function enforceEmail($providers)
     {
         return !empty($providers) ? $providers : ['Two_Factor_Email'];
+    }
+
+    /**
+     * Disable MFA for local/development environments
+     * Return an empty array for development environments, otherwise return $enabled_providers
+     */
+    public function disableForDev($enabled_providers)
+    {
+        if (wp_get_environment_type() === 'development') {
+            return [];
+        }
+        return $enabled_providers;
     }
 }
